c84e0e0dd67551dab66975957ce9b525e913f18a
Yang Heyu <q1651129295@163.com>
2023-12-07 20:53:53 +0800

    test

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    student_name = "John Doe"  # 替换为学生的姓名
-    student_id = "123456"  # 替换为学生的学号
-    photo_url = "/yh"  # 替换为学生的照片 URL
+    student_name = "Yang Heyu"  # 替换为学生的姓名
+    student_id = "75979"  # 替换为学生的学号
+    photo_url = url_for('static', filename='yhy.jpeg')  # 替换为学生的照片 URL
 
     return render_template('index.html', name=student_name, id=student_id, photo_url=photo_url)
 
