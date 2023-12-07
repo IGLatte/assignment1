02dbeddb06241f4af14ead25e00c579a29272da4
Yang Heyu <q1651129295@163.com>
2023-12-07 21:34:14 +0800

    test

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
@@ -9,7 +11,29 @@ def index():
     photo_url = url_for('static', filename='yhy.jpeg')  # 替换为学生的照片 URL
 
     return render_template('index.html', name=student_name, id=student_id, photo_url=photo_url)
-
+@app.route('/reviews')
+def reviews():
+    data = []
+    with open('data/amazon-reviews.csv', 'r') as file:
+        reader = csv.reader(file)
+        first_row = next(reader)
+        for row in reader:
+            photo_row = []
+            for cell in row:
+                if row[0] == '1':
+                    row[0] = photo_row.append(url_for('static', filename='score-1.jpg'))
+                elif row[0] == '2':
+                    row[0] = photo_row.append(url_for('static', filename='score-2.jpg'))
+                elif row[0] == '3':
+                    row[0] = photo_row.append(url_for('static', filename='score-3.jpg'))
+                elif row[0] == '4':
+                    row[0] = photo_row.append(url_for('static', filename='score-4.jpg'))
+                elif row[0] == '5':
+                    row[0] = photo_row.append(url_for('static', filename='score-5.jpg'))
+                else:
+                    photo_row.append(cell)
+            data.append(photo_row)
+    return render_template('reviews.html', data=data, review_head=first_row)
 
 if __name__ == '__main__':
     app.run()
