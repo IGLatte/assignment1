74e92e854b07f269e4d4cbc6a09984a1668760ab
Yang Heyu <q1651129295@163.com>
2023-12-07 20:48:54 +0800

    test

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello W!'
+def index():
+    student_name = "John Doe"  # 替换为学生的姓名
+    student_id = "123456"  # 替换为学生的学号
+    photo_url = "/yh"  # 替换为学生的照片 URL
+
+    return render_template('index.html', name=student_name, id=student_id, photo_url=photo_url)
 
 
 if __name__ == '__main__':
